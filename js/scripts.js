99cfba0fa5275834b5ac01a33e9f1bdcd24c5e42
Jeremy Kale Padot <jeremy@padot.us>
2020-01-24 13:13:58 -0800

    reverse the order of attack. Starting with index splice  3 instead of 1, commented out else if statements for 12, 21, and 32, doesnt work.  dont need it work. *note to self, i feel like the instructions tried to play me*

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,18 @@ var robotResult = function(number) {
   for (index = 0; index <= number; index++){
     result.push(" " + index);
     var string = index.toString();
-     if (string.includes("1")){
-    result.splice(index, 1, " Beep!")
+    // if (string.includes("13")){
+    //   result.splice(index, 1," I'm sorry, Dave. I'm afraid I can't do that." )
+    // } else if (string.includes("21")){
+    // result.splice(index, 1, " Boop!")
+    if (string.includes("3")){
+    result.splice(index, 1, " I'm sorry, Dave. I'm afraid I can't do that.!")
   }  else if (string.includes("2")){
     result.splice(index, 1 ," Boop!")
-  } else if (string.includes("3")){
-    result.splice(index, 1, " I'm sorry, Dave. I'm afraid I can't do that." )
+  } else if (string.includes("1")){
+    result.splice(index, 1, " Beep!" )
+  // } if (string.includes("13")){
+  //   result.splice(index, 1," I'm sorry, Dave. I'm afraid I can't do that." )
   }
 }
   return result;
