79e2f92629812f518bb5bc8db575d7e3ce244c29
Jeremy Kale Padot <jeremy@padot.us>
2020-01-24 11:40:25 -0800

    add working loop for numerical input 0 to users input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
-  $("form#robot").submit(function(event){
-    event.preventDefault();
+var robotResult = function(number) {
+  var result = [];
+  for (index = 0; index <= number; index++){
+    result.push(index);
+  }
+  return result;
+}
+// try switching these
+$(document).ready(function() {
+$("form#robot").submit(function(event) {
+  event.preventDefault();
+  var number = $("#number").val();
+  var result = robotResult(number);
+  // $("#number").val(" ");
+  $("#output").text(result);
+  $("#result").show();
 
-
-    $("#result").show();
   });
 });
+ 
